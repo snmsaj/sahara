347b9c5bf9d36074ac86a710841b2007f826668f
snmsaj <cehd@CEHDs-MacBook-Pro.local>
2021-03-30 17:45:47 -0400

    was able to get water to display

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const models = require('../models')
 
+//Render the water onto the water page
+router.get('/water', (req, res) => {
+    models.Product.findAll({
+        where: {
+            category:'Water'
+        }
+    }).then(water => {
+        res.render('water', {water:water})
+    })
+})
 
 
 module.exports = router;
