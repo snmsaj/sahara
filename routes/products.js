0d00430714d247f320d01bd14410ce6e17f286c6
snmsaj <cehd@CEHDs-MacBook-Pro.local>
2021-03-31 15:43:33 -0400

    page to show all products is complete. Also added links to navbar

--- routes/products.js
+++ routes/products.js
@@ -4,23 +4,36 @@ const models = require("../models");
 
 //Render the water onto the water page
 router.get("/water", (req, res) => {
-  models.Product.findAll({
-    where: {
-      category: "Water",
-    },
-  }).then((water) => {
-    res.render("water", { water: water });
-  });
+    models.Product.findAll({
+        where: {
+            category: "Water",
+        },
+    }).then((water) => {
+        res.render("water", { water: water });
+    });
 });
 
+//Render tea page
 router.get("/tea", (req, res) => {
-  models.Product.findAll({
-    where: {
-      category: "Tea",
-    },
-  }).then((tea) => {
-    res.render("tea", { tea: tea });
-  });
+    models.Product.findAll({
+        where: {
+            category: "Tea",
+        },
+    }).then((tea) => {
+        res.render("tea", { tea: tea });
+    });
 });
+
+//Render all items
+router.get("/", (req, res) => {
+    models.Product.findAll({
+    }).then((items) => {
+        res.render("all-items", { items: items });
+    });
+});
+
+//Goes to page that shows the item you click on
+
+
 
 module.exports = router;
