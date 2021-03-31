f3d97c1fed358dcdc48d6c47ee1c270bdc73b10d
Andres-Andretti <acorredor407@gmail.com>
2021-03-31 10:41:00 -0400

    login and registration completed

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,43 @@ const router = express.Router();
 const models = require('../models')
 
 
+router.post('/', async(req, res) => {
+
+    let username = req.body.username
+    let password = req.body.password
+    
+    let user = await models.User.findOne({
+        where: {
+            username
+        }
+    })
+
+    if(user != null) {
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if(match){
+            //create session 
+            if(req.session) {
+                req.session.user = {userId: user.id}
+                res.redirect('/account/products')
+            }
+        }else{
+            res.render('login', {message: "Incorrect login credentials. Please try again"});
+        }
+
+    } else {
+        res.render('login', {message: "Incorrect login credentials. Please try again"})
+    }
+})
+
+
+router.get('/', (req,res) => {
+    res.render('login')
+})
+
+
+
 router.post('/register', async (req,res) => {
 
     let username = req.body.username
