0b9e3f9d5598f41f6ae5e824b4f377adc22fd6dc
snmsaj <cehd@CEHDs-MacBook-Pro.local>
2021-04-02 01:17:14 -0400

    fixed quantity css again

--- routes/cart.js
+++ routes/cart.js
@@ -5,7 +5,17 @@ const models = require("../models");
 router.get('/', (req, res) => {
     const userId = parseInt(req.session.user.userId)
 
-    
+    models.Cart.findByPk(userId, {
+        include: [
+            {
+                model: models.User,
+                as: 'cartItems'
+            }
+        ]
+    })
+    .then(cartItem => {
+        console.log(cartItem)
+    })
 })
 
 
